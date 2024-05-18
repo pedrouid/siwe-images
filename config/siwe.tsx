29ff25e2b734424a5783b15094b9f8012cc44db3
Pedro Gomes <pedrogomes94@gmail.com>
2024-05-18 21:27:49 +0200

    update siwe.tsx

--- config/siwe.tsx
+++ config/siwe.tsx
@@ -1,35 +1,39 @@
-import { getCsrfToken, signIn, signOut, getSession } from "next-auth/react";
-
 import { SiweMessage } from "siwe";
+
+import { getCsrfToken, signIn, signOut, getSession } from "next-auth/react";
 
 import type {
   SIWEVerifyMessageArgs,
   SIWECreateMessageArgs,
+  SIWESession,
 } from "@web3modal/siwe";
-import { createSIWEConfig } from "@web3modal/siwe";
+
+import { createSIWEConfig, formatMessage } from "@web3modal/siwe";
 
 export const siweConfig = createSIWEConfig({
-  createMessage: ({ nonce, address, chainId }: SIWECreateMessageArgs) =>
-    new SiweMessage({
-      version: "1",
-      domain: window.location.host,
-      uri: window.location.origin,
-      address,
-      chainId,
-      nonce,
-      // Human-readable ASCII assertion that the user will sign, and it must not contain `\n`.
-      statement: "Sign in With Ethereum.",
-    }).prepareMessage(),
+  createMessage: ({ address, ...args }: SIWECreateMessageArgs) =>
+    formatMessage(args, address),
+  getMessageParams: async () => ({
+    domain: window.location.host,
+    uri: window.location.origin,
+    chains: [1],
+    statement: "Please sign with your account",
+  }),
   getNonce: async () => {
     const nonce = await getCsrfToken();
     if (!nonce) {
       throw new Error("Failed to get nonce!");
     }
-
     return nonce;
   },
-  // @ts-ignore
-  getSession,
+  getSession: async () => {
+    const session = await getSession();
+    if (!session) {
+      throw new Error("Failed to get session!");
+    }
+    const { address, chainId } = session as unknown as SIWESession;
+    return { address, chainId };
+  },
   verifyMessage: async ({ message, signature }: SIWEVerifyMessageArgs) => {
     try {
       const success = await signIn("credentials", {
@@ -38,7 +42,6 @@ export const siweConfig = createSIWEConfig({
         signature,
         callbackUrl: "/protected",
       });
-
       return Boolean(success?.ok);
     } catch (error) {
       return false;
@@ -49,7 +52,6 @@ export const siweConfig = createSIWEConfig({
       await signOut({
         redirect: false,
       });
-
       return true;
     } catch (error) {
       return false;
